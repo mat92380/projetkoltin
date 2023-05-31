d93f634eb18d20834d10eb0a95966dd3e7120439
thibault <thibault.collet@epfedu.fr>
2023-05-31 20:48:34 +0200

    Problème récupération des movies

--- app/src/main/java/fr/epfmm/projetmaterielmobile/Movie.kt
+++ app/src/main/java/fr/epfmm/projetmaterielmobile/Movie.kt
@@ -1,3 +1,44 @@
 package fr.epfmm.projetmaterielmobile
 
-data class Movie(val title: String, val release_date: String)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Movie(
+    var adult: Boolean? = false,
+    var overview: String? = "",
+    var releaseDate: String? = "",
+//    var genreIds: ArrayList<Int> = arrayListOf(),
+    var id: Int? = 0,
+    var originalTitle: String? = "",
+    var originalLanguage: String? = "",
+    var title: String? = "",
+    var popularity: Double? = 0.0,
+    var voteCount: Int? = 0,
+    var voteAverage: Double? = 0.0,
+    val release_date: String
+) : Parcelable {
+    companion object{
+
+    }
+
+    override fun toString(): String {
+        return "Movie(id=$id, title=$title)"
+//        return "{" +
+//                "adult='$adult', " +
+//                "overview='$overview', " +
+//                "releaseDate='$releaseDate', " +
+//                "id='$id', " +
+//                "originalTitle='$originalTitle', " +
+//                "originalLanguage=$originalLanguage, " +
+//                "title=$title, " +
+//                "popularity=$popularity, " +
+//                "voteCount=$voteCount, " +
+//                "voteAverage=$voteAverage, " +
+//                "release_date=$release_date " +
+//                "}"
+    }
+
+
+
+}
---
+++ app/src/main/java/fr/epfmm/projetmaterielmobile/MovieService.kt
@@ -0,0 +1,20 @@
+package fr.epfmm.projetmaterielmobile
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieService {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query("api_key") apiKey: String
+    ) : ArrayList<Movie>
+
+    @GET("search/movie")
+    suspend fun getMovies(
+        @Query("api_key") apiKey: String,
+        @Query("query") query: String,
+        @Query("page") page: Int
+    ) : ArrayList<Movie>
+
+}
